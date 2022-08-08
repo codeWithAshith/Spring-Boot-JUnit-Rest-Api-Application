2d577a6c4cf9294a05c25bc1662803798d752710
Ashith V L <ashithvl@gmail.com>
2022-08-08 07:27:13 +0530

    4._Post_Api

--- src/test/java/com/codeWithAshith/SpringBootJUnitRestApiApplication/BookControllerTest.java
+++ src/test/java/com/codeWithAshith/SpringBootJUnitRestApiApplication/BookControllerTest.java
@@ -15,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -43,26 +44,26 @@ public class BookControllerTest {
     @InjectMocks
     private BookController bookController;
 
-    Book RECORD_1 = new Book(1L,"ABCD", "Summary one", 5);
-    Book RECORD_2 = new Book(2L,"EFGH", "Summary two", 4);
-    Book RECORD_3 = new Book(3L,"IJKL", "Summary three", 3);
+    Book RECORD_1 = new Book(1L, "ABCD", "Summary one", 5);
+    Book RECORD_2 = new Book(2L, "EFGH", "Summary two", 4);
+    Book RECORD_3 = new Book(3L, "IJKL", "Summary three", 3);
 
     @Before
-    public void setup(){
+    public void setup() {
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
     }
 
     @Test
     public void getAllBooks_Success() throws Exception {
-        List<Book> bookList = new ArrayList<>(Arrays.asList(RECORD_1,RECORD_2,RECORD_3));
+        List<Book> bookList = new ArrayList<>(Arrays.asList(RECORD_1, RECORD_2, RECORD_3));
 
         Mockito.when(bookRepository.findAll()).thenReturn(bookList);
 
         mockMvc.perform(MockMvcRequestBuilders
-                .get("/book")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(bookList)))
+                        .get("/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookList)))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value("IJKL"));
@@ -73,11 +74,36 @@ public class BookControllerTest {
         Mockito.when(bookRepository.findById(RECORD_1.getBookId())).thenReturn(Optional.of(RECORD_1));
 
         mockMvc.perform(MockMvcRequestBuilders
-                .get("/book/1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(RECORD_1)))
+                        .get("/book/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(RECORD_1)))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", notNullValue()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("ABCD"));
+    }
+
+    @Test
+    public void createBook_Success() throws Exception {
+        Book book = Book.builder()
+                .bookId(4L)
+                .name("name")
+                .summary("sum")
+                .rating(3)
+                .build();
+
+        Mockito.when(bookRepository.save(book)).thenReturn(book);
+
+        String content = objectWriter.writeValueAsString(book);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content);
+
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", notNullValue()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("name"));
     }
 }
